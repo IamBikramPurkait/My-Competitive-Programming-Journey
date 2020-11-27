e226fd30c9404b14f04cad80c9146b13a0d81db2
Bikram <bkrmprkt@gmail.com>
2020-11-27 21:18:41 +0530

    Basic String Programming added

---
+++ #100DaysofDsAlgo/Day 4/stringbasic.cpp
@@ -0,0 +1,36 @@
+// String Basic
+
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    string s="Bikram Purkait";
+    // string b;
+    // char a[10];
+    // cin>>s;
+    // cin>>a;
+    // cout<<a<<endl;
+
+    // getline(cin,b);
+    cout<<s<<endl;
+    // cout<<s.capacity()<<endl;
+    // cout<<s.length()<<endl;
+    // s.push_back('b');
+    // s.pop_back();
+    // s.resize(7);
+    // cout<<s.length()<<endl;
+    // cout<<s.capacity()<<endl;
+    // s.shrink_to_fit();
+    // cout<<s.length()<<endl;
+    // cout<<s.capacity()<<endl;
+    cout<<s<<endl;
+    
+
+
+
+    return 0;
+}
