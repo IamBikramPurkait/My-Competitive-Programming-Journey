312eb445e0ae586170a4d45afa99917031571905
Bikram <bkrmprkt@gmail.com>
2021-02-24 22:39:13 +0530

    AtCoder welcome code added

---
+++ At Coder/Practice/welcomeatcoder.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int a;
+    cin >> a;
+    int b,c;
+    cin >> b >> c;
+    string s;
+    cin >> s;
+    cout << (a+b+c) << " " << s << endl;
+    return 0;
+}
